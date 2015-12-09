81d4ed0ab0cffb48e18c1a224cb528c023264240
Yulia Ten <Y-LyN-10@users.noreply.github.com>
2015-12-09 19:02:49 +0200

    Add comments to init.js

--- bin/init.js
+++ bin/init.js
@@ -2,11 +2,13 @@ var shell = require('./shellHelper');
 
 var exec = require('child_process').exec;
 
+// The browsers are configured to open multiple tabs as home-pages
 var programs = [
-  'chromium',
-  'firefox --browser',
-  'skype',
-  'thunderbird'
+  'chromium',          // open google mail & source control systems
+  'firefox --browser', // open organizers like trello, swipes and my calendar; twitter
+  'skype',             // it just doesn't start automatically on start-up
+  'thunderbird',       // for personal mails & notifications
+  'emacs'              // open the last saved desktop session
 ];
 
 // execute multiple commands in series
